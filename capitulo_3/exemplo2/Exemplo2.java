2dfb0ac62518761a4384c0e074fd449976739aff
AlmirCbBarbosa <almirdelimaestudos@gmail.com>
2023-09-16 20:51:20 -0300

    criado o exemplo 1 e 2 do capitulo 3

---
+++ capitulo_3/exemplo2/Exemplo2.java
@@ -0,0 +1,23 @@
+package capitulo_3.exemplo2;
+//Programa Java mostrando declaração de variáveis
+public class Exemplo2{
+    public static void main(String[] args){
+        int a = 0;
+        int b = 2;
+
+        int soma = a + b;
+        System.out.println("a soma foi: " + soma);
+    }
+}
+
+/*
+    Para executar o programa que você forneceu, siga estas etapas:
+
+        1. Certifique-se de que o nome do arquivo corresponda ao nome da classe. No seu caso, o nome do arquivo deve ser "Exemplo2.java".
+
+        2. Abra um terminal (linha de comando) no diretório onde o arquivo "Exemplo2.java" está localizado.
+
+        3. Compile o programa usando o comando `javac Exemplo2.java`. Isso irá criar um arquivo chamado "Exemplo2.class" no mesmo diretório.
+
+        4. Após a compilação ser bem-sucedida, você pode executar o programa usando o comando `java Exemplo2`. Isso executará a classe "Exemplo2" e produzirá a saída no console.
+*/
---
+++ capitulo_3/exemplo3/Exemplo3.java
@@ -0,0 +1,9 @@
+package capitulo_3.exemplo3;
+
+public class Exemplo3{
+   public static void main(String[] args) {
+        System.out.println("Texto 1");
+
+        System.out.println("Texto 2");
+   }
+}
